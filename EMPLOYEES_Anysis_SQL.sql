2df3460d58416f2d82dd263c591fa332654320e4
Vinay Tekkur <38867261+Vinay-Tekkur@users.noreply.github.com>
2021-09-30 23:34:18 +0530

    Added Students queries
    Added Students and courses related queries from Students DB

--- EMPLOYEES_Anysis_SQL.sql
+++ EMPLOYEES_Anysis_SQL.sql
@@ -109,17 +109,35 @@ ORDER BY salary DESC
 LIMIT 1
 
 
+--------------------------------------------------------- STUDENTS DATABASE QUERIES ------------------------------------------------------------------
+
+-- Using subqueries only, write a SQL statement that returns the names 
+-- of those students that are taking the courses Physics and US History.
+SELECT student_name
+FROM students
+WHERE student_no IN
+(SELECT student_no FROM student_enrollment WHERE course_no IN 
+ (SELECT course_no FROM courses WHERE course_title IN ('Physics', 'US History')))
 
 
+-- Using subqueries only, write a query that returns the name of the student that is taking the 
+-- highest number of courses.
+SELECT student_name
+FROM students
+WHERE student_no IN
+(
+SELECT student_no
+FROM student_enrollment
+WHERE course_no IN (SELECT course_no FROM courses)
+GROUP BY student_no
+ORDER BY COUNT(*) DESC
+LIMIT 1
+)
 
 
-
-
-
-
-
-
-
-
-
+-- Write a query to find the student that is the oldest. 
+-- You are not allowed to use LIMIT or the ORDER BY clause to solve this problem
+SELECT student_name, age
+FROM students
+WHERE age IN (SELECT MAX(age) FROM students)
 
